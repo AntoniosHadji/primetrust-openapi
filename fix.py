ef59251ad7d5fc9d528e442b903b246651fcfa7a
Antonios Hadjigeorgalis <Antonios@Hadji.co>
2021-10-30 15:59:15 -0400

    clean openapi.json file

--- fix.py
+++ fix.py
@@ -1,4 +1,3 @@
-import copy
 import json
 
 
@@ -16,18 +15,26 @@ with open("./primetrust-public-resolved.json", "r") as f:
     data = json.load(f)
 
 print(data.keys())
-final = copy.deepcopy(data)
-# duplicate path with "/v2/resource-tokens/{resource-token-id}"
-del final["paths"]["/v2/resource-tokens/{hash}"]
+# dict_keys(['openapi', 'info', 'paths', 'components', 'servers', 'x-tagGroups', 'tags']) # noqa: E501
+# 	 openapi <class 'str'>
+# 	 info <class 'dict'>
+# 	 paths <class 'dict'>
+# 	 components <class 'dict'>
+# 	 servers <class 'list'>
+# 	 x-tagGroups <class 'list'>
+# 	 tags <class 'list'>
+
+# duplicate paths
 del data["paths"]["/v2/resource-tokens/{hash}"]
+del data["paths"]["/v2/contributions/{hash}"]
 
 # missing parameter
 for p in [
     "/v2/account-cash-transfer-reviews/{account-cash-transfer-review-id}/from-account",
     "/v2/account-cash-transfer-reviews/{account-cash-transfer-review-id}/to-account",
 ]:
-    for method in final["paths"][p]:
-        final["paths"][p][method]["parameters"].append(
+    for method in data["paths"][p]:
+        data["paths"][p][method]["parameters"].append(
             uuid("account-cash-transfer-review-id")
         )
 
@@ -39,39 +46,39 @@ for p in [
     "/v2/accounts/{account-id}/policy",
     "/v2/accounts/{account-id}/sandbox/fund",
 ]:
-    for method in final["paths"][p]:
-        final["paths"][p][method]["parameters"].append(uuid("account-id"))
+    for method in data["paths"][p]:
+        data["paths"][p][method]["parameters"].append(uuid("account-id"))
 
 p = "/v2/account-types/{account-type-id}/policy"
-for method in final["paths"][p]:
-    final["paths"][p][method]["parameters"].append(uuid("account-type-id"))
+for method in data["paths"][p]:
+    data["paths"][p][method]["parameters"].append(uuid("account-type-id"))
 
 p = "/v2/card-transactions/{card-transactions-id}/sandbox"
-for method in final["paths"][p]:
-    final["paths"][p][method]["parameters"].append(uuid("card-transactions-id"))
+for method in data["paths"][p]:
+    data["paths"][p][method]["parameters"].append(uuid("card-transactions-id"))
 
-p = "/v2/contact-funds-transfer-references/{contact-funds-transfer-reference-id}/sandbox/contribution"
-for method in final["paths"][p]:
-    final["paths"][p][method]["parameters"].append(
+p = "/v2/contact-funds-transfer-references/{contact-funds-transfer-reference-id}/sandbox/contribution"  # noqa: E501
+for method in data["paths"][p]:
+    data["paths"][p][method]["parameters"].append(
         uuid("contact-funds-transfer-reference-id")
     )
 
 p = "/v2/funds-transfer-methods/{funds-transfer-method-id}/bank"
-for method in final["paths"][p]:
-    final["paths"][p][method]["parameters"].append(uuid("funds-transfer-method-id"))
+for method in data["paths"][p]:
+    data["paths"][p][method]["parameters"].append(uuid("funds-transfer-method-id"))
 
 for p in [
-    "/v2/internal-asset-transfer-reviews/{internal-asset-transfer-review-id}/from-account",
-    "/v2/internal-asset-transfer-reviews/{internal-asset-transfer-review-id}/to-account",
+    "/v2/internal-asset-transfer-reviews/{internal-asset-transfer-review-id}/from-account",  # noqa: E501
+    "/v2/internal-asset-transfer-reviews/{internal-asset-transfer-review-id}/to-account",  # noqa: E501
 ]:
-    for method in final["paths"][p]:
-        final["paths"][p][method]["parameters"].append(
+    for method in data["paths"][p]:
+        data["paths"][p][method]["parameters"].append(
             uuid("internal-asset-transfer-review-id")
         )
 
 p = "/v2/disbursement-owner-verifications/{resource-token-hash}"
-for method in final["paths"][p]:
-    final["paths"][p][method]["parameters"].append(uuid("resource-token-hash"))
+for method in data["paths"][p]:
+    data["paths"][p][method]["parameters"].append(uuid("resource-token-hash"))
 
 for p in [
     "/v2/resource-tokens/{resource-token-id}/accounts",
@@ -79,30 +86,27 @@ for p in [
     "/v2/resource-tokens/{resource-token-id}/ira-rollover-forms",
     "/v2/resource-tokens/{resource-token-id}/uploaded-documents",
 ]:
-    for method in final["paths"][p]:
-        final["paths"][p][method]["parameters"].append(uuid("resource-token-id"))
+    for method in data["paths"][p]:
+        data["paths"][p][method]["parameters"].append(uuid("resource-token-id"))
 
 for p in [
     "/v2/user-invitation-reviews/{user-invitation-review-id}/from-account",
     "/v2/user-invitation-reviews/{user-invitation-review-id}/to-account",
 ]:
-    for method in final["paths"][p]:
-        final["paths"][p][method]["parameters"].append(
-            uuid("user-invitation-review-id")
-        )
+    for method in data["paths"][p]:
+        data["paths"][p][method]["parameters"].append(uuid("user-invitation-review-id"))
 
-
-for p in data["paths"]:
-    key = "paths"
+key = "paths"
+for p in data[key]:  # noqa: C901
     print("path: ", p)
     for method in data[key][p]:
         k2 = data[key][p][method]
         if "responses" in k2:
             for response in k2["responses"]:
                 if "description" not in k2["responses"][response]:
-                    final[key][p][method]["responses"][response]["description"] = "TODO"
+                    data[key][p][method]["responses"][response]["description"] = "TODO"
                 elif k2["responses"][response]["description"] == "":
-                    final[key][p][method]["responses"][response][
+                    data[key][p][method]["responses"][response][
                         "description"
                     ] = "TODO-was-empty-string"
 
@@ -118,7 +122,7 @@ for p in data["paths"]:
                             "application/vnd.api+json"
                         ]
                     ):
-                        del final[key][p][method]["responses"][response]["content"][
+                        del data[key][p][method]["responses"][response]["content"][
                             "application/vnd.api+json"
                         ]["description"]
 
@@ -138,151 +142,90 @@ for p in data["paths"]:
                             ]["schema"]["title"]
                             is None
                         ):
-                            final[key][p][method]["responses"][response]["content"][
+                            data[key][p][method]["responses"][response]["content"][
                                 "application/vnd.api+json"
                             ]["schema"]["title"] = "string"
 
-for tag in final["tags"]:
+for tag in data["tags"]:
     if tag["description"] == "":
         tag["description"] = "TODO: Fill description."
 
 
-fr = final["components"]["schemas"]["Frequency Rule"]
-del final["components"]["schemas"]["Frequency Rule"]
-final["components"]["schemas"]["Frequency_Rule"] = fr
+fr = data["components"]["schemas"]["Frequency Rule"]
+del data["components"]["schemas"]["Frequency Rule"]
+data["components"]["schemas"]["Frequency_Rule"] = fr
 
-for o in final["components"]["schemas"]["rules__rule_type_info"]["oneOf"]:
+for o in data["components"]["schemas"]["rules__rule_type_info"]["oneOf"]:
     if "$ref" in o:
-        print("found $ref")
         if o["$ref"] == "#/components/schemas/Frequency Rule":
+            print(f"Replace {o['$ref']}")
             o["$ref"] = "#/components/schemas/Frequency_Rule"
 
-# patterns errors
-for o in final["components"]["schemas"]["uploaded_documents__create"]["allOf"]:
-    if o.get("properties") and "extension" in o["properties"]:
-        o["properties"]["extension"]["pattern"] = "/^\.?[a-z]{2,5}$/i"
+
+key = "components"
+subkey = "schemas"
+for c in data[key]:
+    for s in data[key][subkey]:
+        # print(s)
+        if s == "types__date":
+            print(f"Found {s}")
+            print(data[key][subkey][s])
+            data[key][subkey][s]["nullable"] = True
+            print(data[key][subkey][s])
+        if s == "contacts__response":
+            print(f"Found {s}")
+            print(type(data[key][subkey][s]))
+            print(
+                data[key][subkey][s]["properties"]["attributes"]["properties"][
+                    "date-of-birth"
+                ]
+            )
+            if (
+                "nullable"
+                in data[key][subkey][s]["properties"]["attributes"]["properties"][
+                    "date-of-birth"
+                ]
+            ):
+                print("found nullable")
+                del data[key][subkey][s]["properties"]["attributes"]["properties"][
+                    "date-of-birth"
+                ]["nullable"]
+            if (
+                "nullable"
+                in data[key][subkey][s]["properties"]["attributes"]["properties"][
+                    "date-of-incorporation"
+                ]
+            ):
+                print("found nullable")
+                del data[key][subkey][s]["properties"]["attributes"]["properties"][
+                    "date-of-incorporation"
+                ]["nullable"]
+
+        if s == "uploaded_documents__create":
+            print(f"Found {s}")
+            for o in data[key][subkey][s]["allOf"]:
+                if o.get("properties") and "extension" in o["properties"]:
+                    o["properties"]["extension"]["pattern"] = r"/^\.?[a-z]{2,5}$/i"
+
+        if s == "cip_checks__update_idm":
+            print(f"Found {s}")
+            regex = "/(^manual_review_required$)|(^(auto|manual)_check_failed$)/"
+            data[key][subkey][s]["properties"]["exceptions"]["items"]["pattern"] = regex
+
+#  58251:26    error  oas3-schema                    `pattern` property should match format `regex`.                components.schemas.cip_checks__update_idm.properties.exceptions.items.pattern
+#  58251:26    error  oas3-schema                    `pattern` property should match format `regex`.                components.schemas.cip_checks__update_idm.properties.exceptions.items.pattern
+#              "pattern": "(?-mix:(\\Amanual_review_required\\Z)|(((\\A|_)auto|_manual)_check_failed\\Z))"
+# 57957
 
 # converted to javascript \A = ^ \Z = $ multiline off by default
-regex = "/(^manual_review_required$)|(((^|_)auto|_manual)_check_failed$)/"
-final["components"]["schemas"]["cip_checks__update"]["properties"]["exceptions"][
-    "items"
-]["pattern"] = regex
+# patterns errors
+# for o in data["components"]["schemas"]["uploaded_documents__create"]["allOf"]:
+#     if o.get("properties") and "extension" in o["properties"]:
+#         o["properties"]["extension"]["pattern"] = r"/^\.?[a-z]{2,5}$/i"
+#
+# broken as of 2021-10-05
+# regex = "/(^manual_review_required$)|(((^|_)auto|_manual)_check_failed$)/"
 
 
 with open("./openapi.json", "w") as f:
-    json.dump(final, f, indent=2, default=str)
-
-
-#      "cip_checks__update": {
-#        "title": "Cip Checks: Update",
-#        "type": "object",
-#        "properties": {
-#          "exception-details": {
-#            "type": "string"
-#          },
-#          "exceptions": {
-#            "type": "array",
-#            "items": {
-#              "type": "string",
-#              "pattern": "((manual_review_required)|(((|_)auto|_manual)_check_failed))"
-#            }
-#          }
-#        },
-#        "description": ""
-#      },
-
-
-#       "uploaded_documents__create": {
-#         "title": "Uploaded Documents: Create",
-#         "allOf": [
-#           {
-#             "type": "object",
-#             "properties": {
-#               "allow-download": {
-#                 "type": "boolean"
-#               },
-#               "public": {
-#                 "type": "boolean"
-#               },
-#               "label": {
-#                 "type": "string"
-#               },
-#               "description": {
-#                 "type": "string"
-#               }
-#             }
-#           },
-#           {
-#             "type": "object",
-#             "properties": {
-#               "file": {
-#                 "$ref": "#/components/schemas/types__binary"
-#               },
-#               "extension": {
-#                 "type": "string",
-#                 "pattern": "/\.?[a-z]{2,5}/i"
-#               },
-#               "mime-type": {
-#                 "type": "string"
-#               }
-#             },
-#             "required": [
-#               "file"
-#             ]
-#           },
-#           {
-#             "oneOf": [
-#               {
-#                 "type": "object",
-#                 "properties": {
-#                   "account-id": {
-#                     "$ref": "#/components/schemas/types__uuid_v4"
-#                   }
-#                 },
-#                 "required": [
-#                   "account-id"
-#                 ],
-#                 "title": "For an Account"
-#               },
-#               {
-#                 "type": "object",
-#                 "properties": {
-#                   "contact-id": {
-#                     "$ref": "#/components/schemas/types__uuid_v4"
-#                   }
-#                 },
-#                 "required": [
-#                   "contact-id"
-#                 ],
-#                 "title": "For a Contact"
-#               },
-#               {
-#                 "type": "object",
-#                 "properties": {
-#                   "account-risk-review-id": {
-#                     "$ref": "#/components/schemas/types__uuid_v4"
-#                   }
-#                 },
-#                 "required": [
-#                   "account-risk-review-id"
-#                 ],
-#                 "title": "For an Account Risk Review"
-#               },
-#               {
-#                 "type": "object",
-#                 "properties": {
-#                   "funds-transfer-id": {
-#                     "$ref": "#/components/schemas/types__uuid_v4"
-#                   }
-#                 },
-#                 "required": [
-#                   "funds-transfer-id"
-#                 ],
-#                 "title": "For a Funds Transfer"
-#               }
-#             ]
-#           }
-#         ],
-#         "description": ""
-#       },
-#
+    json.dump(data, f, indent=2, default=str)
--- lint-specral-openapi.sh
+++ lint-specral-openapi.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
+rm -f ./*.log
 spectral lint openapi.json &> "openapi.$(date +%FT%H%M).log"
