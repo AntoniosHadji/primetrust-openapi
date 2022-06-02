87a295acfdf9088e8c1de21d80e13ebc9eca13a1
Antonios Hadjigeorgalis <Antonios@Hadji.co>
2022-06-02 19:15:43 -0400

    misc

--- cli-docker-generate.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-# -*- coding: utf-8 -*-
-docker run --rm \
-    -v "$PWD:/local" \
-    openapitools/openapi-generator-cli generate -v -i "/local/${1:-openapi.json}" -g java -o /local/out/java
